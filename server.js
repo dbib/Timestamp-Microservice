34d9c3ec14e44dd1b690f1c5410bc5078d58ec20
dbib <65149459+dbib@users.noreply.github.com>
2022-03-17 14:20:57 +0200

    my Solution to the fcc timestamp challenge

---
+++ server.js
@@ -0,0 +1,59 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+// My soluton
+app.get('/api', (req, res) => {
+  res.json({
+    'unix': new Date().getTime(),
+    'utc': new Date().toUTCString()
+  })
+})
+
+let response = {}
+app.get('/api/:date', (req, res) => {
+  let inputedDate = req.params.date
+
+  if(inputedDate.includes('-')){
+    response['unix'] = new Date(inputedDate).getTime();
+    response['utc'] = new Date(inputedDate).toUTCString();
+  }else {
+    converted = Number(inputedDate);
+    response['unix'] = new Date(converted).getTime();
+    response['utc'] = new Date(converted).toUTCString();
+  }
+
+  if(!response['unix'] || !response['utc']){
+    res.json({
+      'error':'Invalid Date'
+    });
+  }
+  res.json(response);
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
